20ac61292d0988d2bd6c60f01c4dbc688f87487d
6DEADSHOT9 <arora.dhruv26@gmail.com>
2022-10-26 13:27:18 +0530

    System change Q2 started

---
+++ Q2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Person
+{
+
+    char name[64], address[64];
+    int age[64], salary[64];
+
+    int distribution(int i = 0)
+    {
+        for (this->salary[i] = 0; i < 10 ; i++)
+        {
+            /* code */
+        }
+        
+    };
+};
