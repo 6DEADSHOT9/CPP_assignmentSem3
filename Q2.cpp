54a352fb78ae022d4adb5d65603391bfb3ac3586
6DEADSHOT9 <arora.dhruv26@gmail.com>
2022-11-04 13:06:20 +0530

    ALL SHIT DONE

--- Q2.cpp
+++ Q2.cpp
@@ -1,65 +1,79 @@
+// Write inline function that obtains the youngest and eldest age of a person from a class person using arrays.
 #include <iostream>
 using namespace std;
+#define SIZE 10
+using namespace std;
 
-class Person
-{
-
-    public:
-    char name[64], address[64];
-    int age[64], salary[64];
-
-    inline int input(int i = 0)
-    {
-        cout<<"Number of employees: ";
-        int x = 0;
-        cin>>x;
-        for (i = 0; i < x ; i++)
-        {
-            // cout << "Enter name: ";
-            // cin >> name[i];
-            // cout << "Enter age: ";
-            // cin >> age[i];
-            // cout << "Enter address: ";
-            // cin >> address[i];
-            cout << "Enter salary: ";
-            cin >> salary[i];
-        }
-        return 0;
-        
-    };
-
-    int salary_distribution(int x)
-    {
-        int base, hra, ca , ma, da, gross, net;
-        base = (float)salary[x]/2;
-        hra = (float)base*0.45;
-        ca = 1600.0;
-        da = (float)base*0.35;
-        ma = 4000.0;
-        gross = (float)base+hra+ca+da+ma;
-        net = (float)gross-((float)base*0.12);
-        int ret[] = {base, hra, ca, da, ma, gross, net};
-        for (int i = 0; i <= 6; i++)
-        {
-            return ret[i];
-        }
-
-    };
-};
-
-int main()
-{
-    int x = 0, y = 0, salary_arr[7];
-    Person p;
-    p.input(x);
-    cout<<"Enter employee address: ";
-    cin>>y;
-    y = p.salary[y];
-    for (int i = 0; i <= 6; i++)
-    {
-        salary_arr[i] = p.salary_distribution(y);
-        cout<<salary_arr[i]<<endl;
+class Person {
+private:
+    string name, address;
+    int age;
+    float salary;
+public:
+    Person(string name, string address, int age, float salary) {
+        this->name = name;
+        this->address = address;
+        this->age = age;
+        this->salary = salary;
     }
-    // cout<<salary_arr;
+    int getAge() {
+        return age;
+    }
+    void print_salary_slip(){
+        this->name = name;
+        this->address = address;
+        this->age = age;
+        this->salary = salary;
+        cout << "|      Salary slip of " << name << endl;
+        cout << "|      Age: " << age << endl;
+        cout << "|      Address: " << address << endl;
+        cout << "|      Basic Salary: " << salary * 0.5 << endl;
+        cout << "|      HRA: " << salary * 0.3 << endl;
+        cout << "|      DA: " << salary * 0.2 << endl;
+        cout << "|      Total Salary: " << salary << endl;
+        cout << "------------------------------------" << endl;
+    }
+};
+    inline int findYoungest(Person p[], int size) {
+    int min = p[0].getAge();
+    for (int i = 1; i < size; i++) {
+        if (p[i].getAge() < min) {
+            min = p[i].getAge();
+        }  
+    }
+    return min;
+}
+    inline int findOldest(Person p[], int size) {
+    int max = p[0].getAge();
+    for (int i = 1; i < size; i++) {
+        if (p[i].getAge() > max) {
+            max = p[i].getAge();
+        }
+    }
+    return max;
+}
+int main() {
+    Person p[SIZE] = {
+            Person("Maxima Giovanni", "Bridportson", 20, 100000),
+            Person("Aelia Ela", "Ringstourbridge", 30, 200000),
+            Person("Ligia Ylenia", "Ferspear", 40, 300000),
+            Person("Orel Epiphanes", "Coruscant", 50, 98000),
+            Person("Vlasi Lenny", "Troisminster", 60, 87000),
+            Person("Milagros Aoife", "Foysmith", 70, 66000),
+            Person("Nicostrato Zisel", "Druthldenfrecombe", 80, 78000),
+            Person("Elisabeth Emrik", "Moose Lonsneaut", 90, 180000),
+            Person("Igor Volvo", "Grand Maswilkes", 100, 93000),
+            Person("Loren Dove", "Ogeorge", 110, 140000),
+    };
+    int youngest, eldest, k;
+    youngest = findYoungest(p, SIZE);
+    eldest = findOldest(p, SIZE);
+    cout<<"Eldest: "<<eldest<<endl;
+    cout<<"Enter the number of entries to show: ";
+    cin>>k;
+    cout<<"----------------------------------------"<<endl;
+    for(int i = 0; i < k; i++) {
+        p[i].print_salary_slip();
+    }
     return 0;
 }
